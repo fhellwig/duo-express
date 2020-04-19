ffead5203a0670ad8a4e98006bfcc57a4c2eac31
frank.hellwig <frank.hellwig@hii-tsd.com>
2020-04-19 14:02:00 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,141 @@
 # duo-express
+
 Express middleware for handling Duo sign and verify actions
+
+## Purpose
+
+From the client side, you need to create the Duo signature request and then verify the Duo response. Both of these actions require access to the Duo `ikey`, `skey`, and `akey`. These are secret and cannot be part of your web application. Accordingly, these actions must be performed on the server. This package provides an Express middleware module that handles these requests.
+
+## Prerequisites
+
+Some optional functions require that you have installed and configured the [express-session](https://www.npmjs.com/package/express-session) package.
+
+## Installation
+
+```
+npm install --save duo-express
+```
+
+## Usage
+
+```javascript
+const duo = require('duo-express');
+const app = express();
+app.use(duo(duoConfig, paths)); // paths is an optional argument
+```
+
+The optional `paths` argument is a set of paths you want to protect (e.g., `['/api']`). If provided, all requests are checked. If a request path begins with a value specified in the `paths` array and the `/duo/verify` endpoint has not yet been called, then a 401 (Unauthorized) response is returned. As mentioned in the prerequisites, session middleware must be installed for this to work.
+
+## Configuration
+
+This middleware module requires a configuration object having the following properties:
+
+```javascript
+const duoConfig = {
+  ikey: 'your duo integration key',
+  skey: 'your duo secret key',
+  akey: 'your duo application key',
+  host: 'your duo api hostname'
+};
+```
+
+## Endpoints
+
+This middleware module provides three endpoints that you can call from your web application.
+
+### `POST /duo/sign`
+
+This endpoint signs the username using the three Duo keys and returns the request expected by the `Duo.init` method.
+
+```
+POST /duo/sign
+
+{
+  username: 'joe@example.com'
+}
+```
+
+The response is a [JSend](https://github.com/omniti-labs/jsend) object:
+
+```javascript
+{
+  status: 'success',
+  data: {
+    host: 'api-XXXXXXXX.duosecurity.com',
+    request: 'the request that is passed to the duo web init call'
+  }
+}
+```
+
+The hostname is returned so you need only to maintain in one location and not separately in your web app and your server. Having it in the response is a handy way to get it and use it as the `host` parameter to `Duo.init`.
+
+### `POST /duo/verify`
+
+After you display the iFrame and Duo returns a response, the response must be verified.
+
+```
+POST /duo/verify
+
+{
+  response: 'the response from the Duo web iFrame'
+}
+```
+
+The response is a [JSend](https://github.com/omniti-labs/jsend) object:
+
+```javascript
+{
+  status: 'success',
+  data: {
+    username: 'joe@example.com'
+  }
+}
+```
+
+If you have session middleware installed (as evidenced by `req.session` being an object), then a `duo` object is added to the session with the `username` property.
+
+### `GET /duo`
+
+A quick way to check if Duo verification has taken place. The response is a [JSend](https://github.com/omniti-labs/jsend) object:
+
+```javascript
+{
+  status: 'success',
+  data: {
+    username: 'joe@example.com'
+  }
+}
+
+// -or-
+
+{
+  status: 'success',
+  data: null
+}
+```
+
+Again, this will only work if you have session middleware installed.
+
+## License
+
+MIT License
+
+Copyright (c) 2020 Frank Hellwig
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ middleware.js
@@ -0,0 +1,158 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Frank Hellwig
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const duo = require('@duosecurity/duo_web');
+const express = require('express');
+
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+
+function middleware(config, paths) {
+  checkConfig(config);
+
+  const router = new express.Router();
+
+  router.use(express.json());
+
+  router.get('/duo', async (req, res) => {
+    const session = getDuoSession(req);
+    res.json({
+      status: 'success',
+      data: session
+    });
+  });
+
+  router.post('/duo/sign', (req, res) => {
+    const username = req.body.username;
+    if (isString(username)) {
+      const request = duo.sign_request(ikey, skey, akey, username);
+      res.json({
+        host: host,
+        request: request
+      });
+    } else {
+      res.status(BAD_REQUEST);
+      res.json({
+        status: 'fail',
+        code: BAD_REQUEST,
+        data: {
+          username: 'Expected a string for this property'
+        },
+        message: 'Expected a username string property in request'
+      });
+    }
+  });
+
+  router.post('/duo/verify', (req, res) => {
+    const response = req.body.response;
+    if (isString(response)) {
+      const username = duo.verify_response(ikey, skey, akey, response);
+      if (username) {
+        if (isObject(req.session)) {
+          req.session.duo = { username };
+        }
+        res.json({
+          status: 'success',
+          data: {
+            username
+          }
+        });
+      } else {
+        res.status(UNAUTHORIZED);
+        res.json({
+          status: 'error',
+          code: UNAUTHORIZED,
+          message: 'The Duo response could not be verified'
+        });
+      }
+    } else {
+      res.status(BAD_REQUEST);
+      res.json({
+        status: 'fail',
+        code: BAD_REQUEST,
+        data: {
+          response: 'Expected a string for this property'
+        },
+        message: 'Expected a Duo response string property in request'
+      });
+    }
+  });
+
+  if (Array.isArray(paths)) {
+    router.use('*', (req, res, next) => {
+      if (paths.some((p) => req.baseUrl.startsWith(p))) {
+        const session = getDuoSession(req);
+        if (session) {
+          next();
+        } else {
+          res.status(UNAUTHORIZED);
+          res.json({
+            status: 'error',
+            code: UNAUTHORIZED,
+            message: 'The user is not authorized for ' + req.baseUrl
+          });
+        }
+      } else {
+        next();
+      }
+    });
+  }
+
+  return router;
+}
+
+function checkConfig(config) {
+  if (!isObject(config)) {
+    throw new Error('Expected a config object');
+  }
+  if (!isString(config.ikey)) {
+    throw new Error('Expected a string for the config.ikey property');
+  }
+  if (!isString(config.skey)) {
+    throw new Error('Expected a string for the config.skey property');
+  }
+  if (!isString(config.akey)) {
+    throw new Error('Expected a string for the config.akey property');
+  }
+  if (!isString(config.host)) {
+    throw new Error('Expected a string for the config.host property');
+  }
+}
+
+function getDuoSession(req) {
+  if (!isObject(req.session)) {
+    throw new Error('This middleware requires a session to work');
+  }
+  return req.session.duo || null;
+}
+
+function isObject(obj) {
+  return obj !== null && typeof obj === 'object';
+}
+
+function isString(str, minLength = 1) {
+  return typeof str === 'string' && str.length >= minLength;
+}
+
+module.exports = middleware;
